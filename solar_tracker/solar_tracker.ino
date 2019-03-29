5e028331a08efe9a14e4395eed65a2d8d00c6a45
ZWGroce <47144376+ZWGroce@users.noreply.github.com>
2019-03-28 22:59:11 -0400

    Functional Update
    Add sweep function, encapsulate tracking in a separate function.  Adds automatic calibration (through a sweep) and LED functionality

--- solar_tracker/solar_tracker.ino
+++ solar_tracker/solar_tracker.ino
@@ -1,36 +1,72 @@
-#include <Servo.h>
-#include <Wire.h>
+#include <Servo.h>  //Includes a library for servo motor functionality
+#include <Wire.h>  //Includes a library to utilize I2C
 //Task: Make a solar tracker
-Servo vert;
-Servo horz;
+Servo vert;  //Creates an object for the servo controlling the pitch
+Servo horz;  //Creates an object for the servo controlling the yaw
  
 //*****Define Variables*****
 const int t_left = A0, t_right = A1, b_left = A2, b_right = A3;  //LDR Matrix pins
 
-//LDR filtering stuff
-float fc = 1, h = 0.02;  //fc = cutoff frequency [Hz], h = sampling time [s]
-float wc = 2*PI*fc, a = 1-wc*h, b = wc*h;  //wc = fc in rad/s
-
-float tleft, tright, bleft, bright;  //raw LDR values 
-float tleftf, trightf, bleftf, brightf;  //filtered LDR values
+float tleft, tright, bleft, bright;  //raw LDR values
+float tleftMax, trightMax, bleftMax, brightMax;
 
 float top, bottom, left, right;  
-int twitchBuffer = 2;
-int vertPosition = 100, horzPosition = 90;
+int twitchBuffer = 3;
+int vertPosition = 30, horzPosition = 105;
+
+bool pitch = LOW, yaw = LOW;  //True means increment, false means deccrement
+
+const int ledPin = 7;
+
+int startTime;
 
 //*****Program Initialization*****
 void setup() {
   vert.attach(9);
   horz.attach(10);
-  pinMode(t_left, INPUT);
-  pinMode(t_right, INPUT);
-  pinMode(b_left, INPUT);
-  pinMode(b_right, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 
   vert.write(vertPosition);
   horz.write(horzPosition);
   
   Serial.begin(9600);
+  delay(1000);
+
+  startTime = millis();  //Updates the start time
+  while((millis()-startTime) < 4000){  //Runs while the time since starting calibration is less than 7 seconds
+    tleft = analogRead(t_left);  //Reads the value of the left LDR
+    tright = analogRead(t_right);  //Reads the value of the center LDR
+    bleft = analogRead(b_left);  //Reads the value of the right LDR
+    bright = analogRead(b_right);
+
+    if(tleft > tleftMax){  //Triggers if the left LDR is greater than the maximum value (starts at 0)
+      tleftMax = tleft;  //Sets the maximum value equal to the current (larger) LDR reading
+    }
+
+    if(tright > trightMax){  //Triggers if the right LDR is greater than the maximum value (starts at 0)
+      trightMax = tright;  //Sets the maximum value equal to the current (larger) LDR reading  
+    }
+
+    if(bleft > bleftMax){  //Triggers if the left LDR is greater than the maximum value (starts at 0)
+      bleftMax = bleft;  //Sets the maximum value equal to the current (larger) LDR reading
+    }
+
+    if(bright > brightMax){  //Triggers if the right LDR is greater than the maximum value (starts at 0)
+      brightMax = bright;  //Sets the maximum value equal to the current (larger) LDR reading  
+    }
+    
+    sweep();
+    
+    delay(20);  //Halts the program for 20 milliseconds
+  }
+
+  tleftMax += 5;
+  trightMax += 5;
+  bleftMax += 5;
+  brightMax += 5;
+  
+  delay(1000);
 }
 
 //*****Main Program*****
@@ -40,43 +76,80 @@ void loop() {
   bleft = analogRead(b_left);
   bright = analogRead(b_right);
 
-  tleftf = a*tleftf + b*tleft;
-  trightf = a*trightf + b*tright;
-  bleftf = a*bleftf + b*bleft;
-  brightf = a*brightf + b*bright;
+  if(tleft <= tleftMax && tright <= trightMax && bleft <= bleftMax && bright <= brightMax){
+    digitalWrite(ledPin, LOW);
+    sweep();
+    digitalWrite(ledPin, HIGH);
+    delay(5);
+    digitalWrite(ledPin, LOW);
+    delay(10);
+  }else{
+    track();
+    digitalWrite(ledPin, HIGH);
+    delay(10);
+  }
+}
 
-  top = (tleftf + trightf)/2;
-  bottom = (bleftf + brightf)/2;
-  left = (tleftf + bleftf)/2;
-  right = (trightf + brightf)/2;
+void sweep(){  
+  if(pitch == HIGH){
+    vertPosition += 1;
+    if(vertPosition >= 70){
+      vertPosition = constrain(vertPosition, 0, 180);
+      pitch = LOW;
+    }
+  }else{
+    vertPosition -= 1;
+    if(vertPosition <= 5){
+      vertPosition = constrain(vertPosition, 0, 180);
+      pitch = HIGH;
+    }  
+  }
 
-  Serial.print(tleft); Serial.print("\t"); Serial.println(tright);
-  Serial.print(bleft); Serial.print("\t"); Serial.println(bright); Serial.println();
-  
-  /*if(abs(top - bottom) > twitchBuffer){
+  if(yaw == HIGH){
+    horzPosition += 2;
+    if(horzPosition >= 150){
+      horzPosition = constrain(horzPosition, 0, 180);
+      yaw = LOW;
+    }
+  }else{
+    horzPosition -= 2;
+    if(horzPosition <= 60){
+      horzPosition = constrain(horzPosition, 0, 180);
+      yaw = HIGH;
+    }  
+  }
+
+  vert.write(vertPosition);
+  horz.write(horzPosition);
+}
+
+void track(){
+  top = (tleft + tright)/2;
+  bottom = (bleft + bright)/2;
+  left = (tleft + bleft)/2;
+  right = (tright + bright)/2;
+   
+  if(abs(top - bottom) > twitchBuffer){
     if(top > bottom){
-      vertPosition++;
-      vertPosition = constrain(vertPosition, 20, 180);
+      vertPosition--;
+      vertPosition = constrain(vertPosition, 5, 100);
       vert.write(vertPosition);    
     }else{
-      vertPosition--;
-      vertPosition = constrain(vertPosition, 20, 180);
+      vertPosition++;
+      vertPosition = constrain(vertPosition, 5, 100);
       vert.write(vertPosition);  
     }  
   }
 
   if(abs(left - right) > twitchBuffer){
     if(left > right){
-      horzPosition--;
+      horzPosition++;
       horzPosition = constrain(horzPosition, 0, 180);
       horz.write(horzPosition); 
     }else{
-      horzPosition++;
+      horzPosition--;
       horzPosition = constrain(horzPosition, 0, 180);
       horz.write(horzPosition);  
     }
-  }
-
-  delay(20);  //Might need to be lowered, needed to be about 30 to 40 for a large range sweep, but can probably be lower for small adjustments
-  */
+  }  
 }
